fc1822b68b02c2e9a4929c427c826e789bcef390
Michael Chapman <woppin@gmail.com>
2014-02-10 14:14:00 +1100

    Initial commit

---
+++ manifests/master.pp
@@ -0,0 +1,67 @@
+#
+# this profile configures a machine
+# as a puppetmaster.
+#
+# [*puppetlabs_repo*]
+# (optional) Sets the apt/yum repository from which
+# puppet master will be installed to be puppetlabs
+# Default: true
+
+class stacktira::master (
+  puppetlabs_repo = true,
+  puppet_master_address = $::fqdn
+)
+
+  $puppet_master_bind_address = $puppet_master_address'
+# installs puppet
+# I think I want to assume a puppet 3.x install
+
+# if this is not set, make sure nodes are all pointing at an apt/yum repo
+# that has puppet > 3.2
+  if $puppetlabs_repo {
+    include puppet::repo::puppetlabs
+  }
+
+  include apache
+
+# the puppetdb package should not be installed
+# before our certificate is generated
+  Exec['Certificate_Check'] -> Package['puppetdb']
+
+# we need to validate the puppetdb http connection
+# and not the https one b/c will not have access to
+# the certificate b/c we create it during the puppet
+# run
+  Service['puppetdb'] -> Puppetdb_conn_validator['puppetdb_conn_http']
+  puppetdb_conn_validator { 'puppetdb_conn_http':
+    ensure => present,
+    puppetdb_server => $puppet_master_bind_address,
+    puppetdb_port => 8080,
+    use_ssl => false,
+    timeout => 240,
+    notify => Class['apache'],
+  }
+
+# install puppet master
+  class { '::puppet::master':
+    certname => $::fqdn,
+    autosign => true,
+    modulepath => '/etc/puppet/modules:/usr/share/puppet/modules',
+  }
+
+# install puppetdb and postgresql
+  class { 'puppetdb':
+    listen_address => $puppet_master_bind_address,
+    ssl_listen_address => $puppet_master_bind_address,
+    database_password => 'datapass',
+  }
+
+# Configure the puppet master to use puppetdb.
+  class { 'puppetdb::master::config':
+    puppetdb_server => $puppet_master_bind_address,
+    puppetdb_port => 8081,
+    restart_puppet => false,
+# I only want to validate with http
+    strict_validation => false,
+  }
+}
