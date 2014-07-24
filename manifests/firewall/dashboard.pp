34a70a5e794aff782624461c1b5bd30e761b41df
Michael Chapman <woppin@gmail.com>
2014-07-24 11:18:39 +1000

    Add support for dashboard SSL
    This patch adds a simple option to enable SSL on the dashboard
    that will have port 443 at the front and port 80 at the back,
    plus a redirect for port 80 to port 443.
    
    The SSL cert path should be specified either as part of a list
    of bind_options or via the convenience parameter provided.
    
    An SSL port has also been opened in the dashboard firewall rule.

--- manifests/firewall/dashboard.pp
+++ manifests/firewall/dashboard.pp
@@ -5,23 +5,30 @@
 # [*source*]
 #   (required) Source CIDR to allow connections from
 #
-# [*internal_source*]
-#   (optional) If set, additional source CIDR to allow
-#   connections from. Use for internal endpoint.
-#   Defaults to undef
-#
 # [*httpd_port*]
 #   (optional) The port to open for httpd
 #   Defaults to 80
 #
+# [*https_port*]
+#   (optional) The port to open for httpd
+#   Defaults to 443
+#
 class openstacklib::firewall::dashboard(
   $source,
   $httpd_port     = 80,
+  $https_port     = 443,
 )
 {
-  firewall { '560 dashboard-httpd accept tcp':
+  firewall { '560 dashboard-http accept tcp':
     proto  => 'tcp',
     port   => [$httpd_port],
+    action => accept,
+    source => $source
+  }
+
+  firewall { '561 dashboard-https accept tcp':
+    proto  => 'tcp',
+    port   => [$https_port],
     action => accept,
     source => $source
   }
